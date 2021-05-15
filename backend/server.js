8737aba689acabb51e452819dafe66db6b188923
Samaresh Samanta <ss9261@srmist.edu.in>
2021-05-16 02:06:39 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -12,3 +12,15 @@ app.use(cors());
 app.listen(port, ()=>{
     console.log(`server running on Port :- ${port}`);
 });
+
+
+//---------->for the extraction of texts<----------
+
+//https://www.npmjs.com/package/pdf-extraction ---------->Extract text from pdf
+//https://www.npmjs.com/package/node-goose-honk ---------->Extract text from html pages
+//https://www.npmjs.com/package/office-text-extractor ---------->Extract text from any word doc
+
+
+//---------->for the summarizations of texts<----------
+
+//https://www.npmjs.com/package/nodejs-text-summarizer
