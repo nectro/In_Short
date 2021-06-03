266670e267b928288bc8be24a900d3a06298d5a8
Samaresh Samanta <ss9261@srmist.edu.in>
2021-06-03 19:58:55 +0530

    added the landing page styles for desktop

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import styles from './Landing.module.css'
+import Pic from '../../assets/img1.svg'
+import { Link } from 'react-router-dom'
 
 const Landing = ()=>{
     return(
-        <div>
-            landing
+        <div className={styles.majorContainer}>
+            <div className={styles.infoContainer}>
+                <div className={styles.heading}>
+                    <h1>ONLINE<br/> SUMMARIZER<br/> AND SPELLCHECKER</h1>
+                </div>
+                <div className={styles.description}>
+                    <p>Tired of going through the study materials marking important points or the same when you are continously going through an article checking for spelling mistake before posting it online. well, here we have a solution to get out of that hustle</p>
+                </div>
+                <div className={styles.buttonContainer}>
+                    <Link to="/Summarizer" className={styles.btn1}>Use Summarizer</Link>
+                    <Link to="/Spellchecker" className={styles.btn2}>Use Spell Check</Link>
+                </div>
+
+            </div>
+            <div className={styles.photoContainer}>
+                <img src={Pic} />
+            </div>
         </div>
     )
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import styles from './Navbar.module.css'
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+
 
 const Navbar = ()=>{
     return(
         <div className={styles.majorContainer}>
+            <div className={styles.options}>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
             <div className={styles.logoContainer}>
                 <p>Simplify</p>
+            </div>
+            <div className={styles.options}>
+                <FontAwesomeIcon icon={faInfoCircle} />
             </div>
             <div className={styles.menuContainer}>
                 <ul>
